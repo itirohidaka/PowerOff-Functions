0463ef0489f52e2c5eb901d0402b55042ddbdd13
Gerson Itiro Hidaka <itirohidaka@msn.com>
2019-08-21 13:07:45 -0300

    code changed. waitgroup implemented

--- golang_version/main.go
+++ golang_version/main.go
@@ -1,14 +1,12 @@
-/*
-*
-* Golang version of PowerOff/On VSI
-* Use the followin example as a Trigger JSON Output
-{
-  "username": "1234567_itiro@br.ibm.com",
-  "key": "dfasdflasdfhjkahsdkflhaksdhjfklhaksjdhfklahskjfhakshdfkj",
-  "vsiname":"itiroteste02",
-  "poweraction":"off"
-}
-*/
+/**
+ *
+ * main() will be run when you invoke this action
+ *
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * @return The output of this action, which must be a JSON object.
+ *
+ */
 package main
 
 import (
@@ -18,8 +16,10 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
+// Main is the function implementing the action
 func Main(params map[string]interface{}) map[string]interface{} {
 	// parse the input JSON
 	username, ok := params["username"].(string)
@@ -30,25 +30,31 @@ func Main(params map[string]interface{}) map[string]interface{} {
 	if !ok {
 		apikey = "None"
 	}
-	vsiname, ok := params["vsiname"].(string)
-	if !ok {
-		vsiname = "None"
-	}
 	poweraction, ok := params["poweraction"].(string)
 	if !ok {
 		poweraction = "None"
 	}
+	tag, ok := params["tag"].(string)
+	if !ok {
+		tag = "None"
+	}
+	fmt.Println(params)
 
-	// building the return msg
 	msg := make(map[string]interface{})
 	msg["username"] = username
 	msg["key"] = apikey
-	msg["vsiname"] = vsiname
+	msg["tag"] = tag
 	msg["poweraction"] = poweraction
 
+	// can optionally log to stdout (or stderr)
+	fmt.Println(username)
+	fmt.Println(apikey)
+
+	fmt.Println("Starting Functions...")
+
 	// get the list of all VSIs from IBM Cloud account
-	fmt.Println("getting the list of VSIs...")
-	url := "https://api.softlayer.com/rest/v3.1/SoftLayer_Account/getVirtualGuests"
+	fmt.Println("Getting the list of VSIs...")
+	url := "https://api.softlayer.com/rest/v3/SoftLayer_Account/getVirtualGuests?objectFilter={\"virtualGuests\":{\"tagReferences\":{\"tag\":{\"name\":{\"operation\":\"in\",\"options\":[{\"name\":\"data\",\"value\":[\"" + tag + "\"]}]}}}}}"
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	req.SetBasicAuth(username, apikey)
@@ -58,42 +64,49 @@ func Main(params map[string]interface{}) map[string]interface{} {
 		fmt.Println(err)
 	}
 	bodyText, err := ioutil.ReadAll(resp.Body)
+	msg["body"] = string(bodyText)
 
-	// finding the VSI ID
-	fmt.Println("finding the VSI ID using hostname...")
+	// Acting on the VSIs
+	fmt.Println("Starting the Power Action on VSIs...")
 	var vsiId float64
 	var vsi []map[string]interface{}
-	var hostname string
+	var vsiIdStr string
+	var vsiname string
 	json.Unmarshal([]byte(bodyText), &vsi)
 	for _, result := range vsi {
-		hostname, _ = result["hostname"].(string)
-		if hostname == vsiname {
-			vsiId = result["id"].(float64)
-		}
-	}
-	var vsiIdStr string = strconv.Itoa(int(vsiId))
-	fmt.Printf("Id: %s ( %s )\n", vsiIdStr, vsiname)
-	msg["vsiid"] = vsiIdStr // including the vsiID into the return msg
+		vsiname, _ = result["hostname"].(string)
+		vsiId = result["id"].(float64)
+		vsiIdStr = strconv.Itoa(int(vsiId))
+		fmt.Printf("Id: %s ( %s )\n", vsiIdStr, vsiname)
 
-	// make the request to IBM Cloud api / SoftLayer api
-	fmt.Println("sending the http request...")
+		//goroutine function
+		go say(poweraction, vsiIdStr, username, apikey)
+	}
+	fmt.Println("End.")
+
+	//say(poweraction,vsiIdStr, username, apikey)
+	fmt.Println("Sleeping...")
+	time.Sleep(10000 * time.Millisecond)
+	return msg
+}
+
+func say(poweraction string, vsiIdStr string, username string, apikey string) {
+	var url string
+
 	if poweraction == "on" {
 		url = "https://api.softlayer.com/rest/v3.1/SoftLayer_Virtual_Guest/" + vsiIdStr + "/powerOn"
 	} else {
 		url = "https://api.softlayer.com/rest/v3.1/SoftLayer_Virtual_Guest/" + vsiIdStr + "/powerOff"
 	}
-	client = &http.Client{}
-	req, err = http.NewRequest("GET", url, nil)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
 	req.SetBasicAuth(username, apikey)
-	resp, err = client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 		fmt.Println(err)
 	}
-	bodyText, err = ioutil.ReadAll(resp.Body)
+	fmt.Println(vsiIdStr)
+	bodyText, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(bodyText))
-	fmt.Println("End.")
-
-	// return the output JSON
-	return msg
 }
